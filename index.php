95ef4b9c82bb7b6dab5271c966700377b9fd0b7e
CarloSalaris <carloa.salaris@gmail.com>
2023-07-03 10:12:22 +0200

    Prima parte correzione in classe (accettare array di generi e trasformarlo in stringa con metodo)

--- index.php
+++ index.php
@@ -7,30 +7,46 @@ class Movie {
     public $genre;
     public $language;
 
-    public function __construct($title, $year, $genre, $language) { //COSTRUTTORE
+    public function __construct($title, $year, $language, ...$genre) { //COSTRUTTORE
+        
         $this -> title = $title;
         $this -> year = $year;
-        $this -> genre = $genre;
         $this -> language = $language;
+
+        $this -> genre = $genre;
+
     }
 
-    public function getTitle(){
-        return "Title: " . $this -> title;
+    public function getGenreStr() { //METODO PER ESTRARRE STRINGA DALL'ARRAY GENRE
+
+        $res = "";
+        for ($i=0; $i < count($this -> genre); $i++) { 
+            
+            $genre = $this -> genre[$i];
+            $res .= $genre . (
+                $i < count($this -> genre) - 1
+                ? ", "
+                : ""
+            );
+        }
+
+        return $res;
     }
 
-    public function getMovieInfo(){
+    public function getMovieInfo() {
+        
         return "Title: " . $this -> title . "<br />" . 
                 "Year: " . $this -> year . "<br />" .
-                "Genre: " . $this -> genre . "<br />" . 
+                "Genre: " . $this -> getGenreStr() . "<br />" . //STRINGA ESTRATTA DALL'ARRAY GENRE
                 "Language: " . $this -> language . "<br />";
 
     }
 
 }
 
-$movie1 = new Movie("Pulp Fiction", "1994","Pulp", "American-english");
+$movie1 = new Movie("Pulp Fiction", "1994", "American-english", "Pulp", "Action");
 
-$movie2 = new Movie("The Big Lebowski", "1998", "comedy", "American-english");
+$movie2 = new Movie("The Big Lebowski", "1998", "American-english", "comedy");
 
 echo "<pre>";
 var_dump($movie1);
@@ -42,7 +58,7 @@ echo $movie1 -> getMovieInfo();
 echo "<br>";
 
 echo "<br><br>";
-echo $movie2 -> getTitle();
+echo $movie2 -> getMovieInfo();
 
 
 
